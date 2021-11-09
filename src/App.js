871a9fbfde8fe910ab0d666861962962328ad632
Kerilet <jivore78@gmail.com>
2021-11-09 10:26:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Todo App will soon replace this.
         </p>
         <a
           className="App-link"
