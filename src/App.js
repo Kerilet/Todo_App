545e833f743a1101c11ce740747b5c6e9a375fb5
Kerilet <jivore78@gmail.com>
2021-11-10 19:43:14 -0300

    temporary

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import style from './App.module.css';
 
 function App() {
+  const num = Math.floor(Math.random() * 30);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Todo App will soon replace this.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.App}>
+      <div className={style.todoTitle}>T O D O</div>
+      <div><img src="./icon-sun.svg" alt="desktop-sun" className={style.sunDesktop}/> </div>
+      <div><img src="./bg-desktop-dark.jpg" alt="bg-desktop-dark" className={style.bgDesktop}/></div>
+
+      <div className={style.todoInput}>
+        <input type="text"></input>
+      </div>
+
+      <div>
+        <div>{num} items left</div>
+        Clear Completed
+      </div>
+
+      <div className="optionsContainer">
+        <div>All</div>
+        <div>Active</div>
+        <div>Completed</div>         
+      </div>
+
+
+      <div>Drag and drop to reorder list</div>
+
+      <div className={style.attribution}>
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+        Coded by Kerilet.
+      </div>
     </div>
   );
 }
