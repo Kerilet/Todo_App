cb010f566c8701aa419df4e9a6c08601971585da
Kerilet <jivore78@gmail.com>
2021-11-11 17:24:32 -0300

    checkbox in progress;

--- src/App.js
+++ src/App.js
@@ -7,22 +7,29 @@ function App() {
     <div className={style.App}>
       <div className={style.todoTitle}>T O D O</div>
       <div><img src="./icon-sun.svg" alt="desktop-sun" className={style.sunDesktop}/> </div>
-      <div><img src="./bg-desktop-dark.jpg" alt="bg-desktop-dark" className={style.bgDesktop}/></div>
-
       <div className={style.todoInput}>
         <input type="text"></input>
       </div>
+      <div><img src="./bg-desktop-dark.jpg" alt="bg-desktop-dark" className={style.bgDesktop}/></div>
 
       <div>
         <div>{num} items left</div>
-        Clear Completed
+        <div className={style.todoList}>
+          <ul>
+            <li><input type="checkbox" className={style.todoCheckbox} />Task Number One</li>
+            <li><input type="checkbox" className={style.todoCheckbox} />Task Number Two</li>
+            <li><input type="checkbox" className={style.todoCheckbox} />Task Number Three</li>
+            <li><input type="checkbox" className={style.todoCheckbox} />Task Number Four</li>
+          </ul>
+        </div>
+        <div className="optionsContainer">
+          <div>All</div>
+          <div>Active</div>
+          <div>Completed</div>         
+        </div>
+        <div>Clear Completed</div>
       </div>
 
-      <div className="optionsContainer">
-        <div>All</div>
-        <div>Active</div>
-        <div>Completed</div>         
-      </div>
 
 
       <div>Drag and drop to reorder list</div>
